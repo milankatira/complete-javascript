f1eb27f171b4345bdb81a42b43ca4a3a2ff41c1e
milankatira <milankatira26@gmail.com>
2021-09-19 14:32:26 +0530

    questions

--- questions/que-9.js
+++ questions/que-9.js
@@ -1,2 +1,23 @@
 const arr=[
-['zone-1','zone-2']]
+['zone-1','zone-2'],
+['zone-3','zone-4'],
+['zone-5','zone-6'],
+['zone-7',['zone-8','zone-9'],]]
+
+
+const flatarr=arr.reduce((accum,curval)=>{
+return accum.concat(curval);
+
+})
+
+console.log(flatarr);
+
+
+//after  es 2020
+const arr_1=[
+    ['zone-1','zone-2'],
+    ['zone-3','zone-4'],
+    ['zone-5','zone-6'],
+    ['zone-7',['zone-8','zone-9'],]]
+    
+console.log(arr_1.flat());
